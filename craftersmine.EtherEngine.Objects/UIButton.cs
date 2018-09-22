742f519f55db3f3a3f3be0e1d5c884934baf5a63
craftersmine <rodion.morozov.01@gmail.com>
2018-09-22 14:05:58 +0700

    Updated documentaion

--- craftersmine.EtherEngine.Objects/UIButton.cs
+++ craftersmine.EtherEngine.Objects/UIButton.cs
@@ -10,19 +10,36 @@ using craftersmine.EtherEngine.Core;
 
 namespace craftersmine.EtherEngine.Objects
 {
-    public sealed class UIButton : UIControl
+    /// <summary>
+    /// Represents UI Button
+    /// </summary>
+    public class UIButton : UIControl
     {
         private Texture UsedTexture { get; set; }
         private int ButtonHeight { get; set; }
         private bool IsMouseHover { get; set; }
+        /// <summary>
+        /// Gets or sets the button reacts on click event
+        /// </summary>
         public bool IsEnabled { get; set; }
 
+        /// <summary>
+        /// Creates new UI button instance with specified size and texture
+        /// </summary>
+        /// <param name="width">Width of button</param>
+        /// <param name="height">Height of button</param>
+        /// <param name="texture">Button texture</param>
+        /// <param name="buttonHeight">Button state image height in texture</param>
         public UIButton(int width, int height, Texture texture, int buttonHeight) : base(width, height)
         {
             UsedTexture = texture;
             ButtonHeight = buttonHeight;
         }
 
+        /// <summary>
+        /// Calls on render call. Use to define how button draws
+        /// </summary>
+        /// <param name="graphics">Painter</param>
         public override void DrawMethod(Graphics graphics)
         {
             base.DrawMethod(graphics);
@@ -31,11 +48,22 @@ namespace craftersmine.EtherEngine.Objects
             IsMouseHover = false;
         }
 
+        /// <summary>
+        /// Calls on mouse click on button
+        /// </summary>
+        /// <param name="button">Mouse button</param>
+        /// <param name="x">Global position of click by X axis</param>
+        /// <param name="y">Global position of click by Y axis</param>
         public override void OnMouseClick(MouseButtons button, int x, int y)
         {
             base.OnMouseClick(button, x, y);
         }
 
+        /// <summary>
+        /// Calls when mouse hovering on button
+        /// </summary>
+        /// <param name="xMousePos">Global position of mouse position by X axis</param>
+        /// <param name="yMousePos">Global position of mouse position by Y axis</param>
         public override void OnMouseHover(int xMousePos, int yMousePos)
         {
             base.OnMouseHover(xMousePos, yMousePos);
--- craftersmine.EtherEngine.Utils/Vector2.cs
+++ craftersmine.EtherEngine.Utils/Vector2.cs
@@ -6,17 +6,37 @@ using System.Threading.Tasks;
 
 namespace craftersmine.EtherEngine.Utils
 {
+    /// <summary>
+    /// Represents 2-dimentional vector
+    /// </summary>
     public struct Vector2
     {
+        /// <summary>
+        /// Gets X axis coordinate of vector destination point
+        /// </summary>
         public int X { get; internal set; }
+        /// <summary>
+        /// Gets Y axis coordinate of vector destination point
+        /// </summary>
         public int Y { get; internal set; }
 
+        /// <summary>
+        /// Creates new <see cref="Vector2"/> instance with specified destination point coordinates
+        /// </summary>
+        /// <param name="x">X axis coordinate of vector destination point</param>
+        /// <param name="y">Y axis coordinate of vector destination point</param>
         public Vector2(int x, int y)
         {
             X = x;
             Y = y;
         }
 
+        /// <summary>
+        /// Adds two vectors into one
+        /// </summary>
+        /// <param name="vec1">First <see cref="Vector2"/></param>
+        /// <param name="vec2">Second <see cref="Vector2"/></param>
+        /// <returns></returns>
         public static Vector2 operator +(Vector2 vec1, Vector2 vec2)
         {
             vec1.X += vec2.X;
@@ -24,6 +44,12 @@ namespace craftersmine.EtherEngine.Utils
             return vec1;
         }
 
+        /// <summary>
+        /// Subtracts one vector from another vector
+        /// </summary>
+        /// <param name="vec1">Decreasing vector</param>
+        /// <param name="vec2">Subtract vector</param>
+        /// <returns></returns>
         public static Vector2 operator -(Vector2 vec1, Vector2 vec2)
         {
             vec1.X -= vec2.X;
