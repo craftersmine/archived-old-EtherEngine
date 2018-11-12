8c8b3ef7606b20eb2c11d6e8a475dd9096514092
Rodion <rodion.morozov.01@gmail.com>
2018-11-12 23:06:29 +0700

    Updated Transform

--- craftersmine.EtherEngine.Core/Transform.cs
+++ craftersmine.EtherEngine.Core/Transform.cs
@@ -1,28 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace craftersmine.EtherEngine.Core
 {
     public sealed class Transform
     {
-        private Vector2 _pos;
-        public Rectangle Bounds { get; set; }
-        public Vector2 Position { get { return _pos; } set { _pos = value; CameraX += _pos.X; CameraY += _pos.Y; } }
+        public Rect Bounds { get; set; }
+        public Vector2 Position { get; set; }
 
-        internal double CameraX { get; set; }
-        internal double CameraY { get; set; }
+        internal Vector2 RelativeCameraPosition { get; set; }
 
-        public Transform(Rectangle bounds, Vector2 position)
+        public Transform(Rect bounds, Vector2 position)
         {
             Bounds = bounds;
             Position = position;
         }
 
-        public Transform(Rectangle bounds) : this(bounds, Vector2.Empty)
+        public Transform(Rect bounds) : this(bounds, Vector2.Empty)
         {}
+
+        internal void UpdateCoordsRelativeViewport(Transform cameraTransform)
+        {
+            RelativeCameraPosition = Position + cameraTransform.Position;
+        }
     }
 }
