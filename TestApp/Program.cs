d87a8f081d920d5acf7e59855ed21d3c9b42d468
Rodion <rodion.morozov.01@gmail.com>
2018-11-12 23:09:37 +0700

    Developer changes

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Media;
 using craftersmine.EtherEngine.Core;
@@ -29,7 +31,6 @@ namespace TestApp
     {
         public Wnd() : base ("TestApp", new WindowSize(800, 600))
         {
-            
         }
 
         public override void OnMouseUp(MouseEventArguments args)
@@ -39,6 +40,7 @@ namespace TestApp
 
         public override void OnWindowShown()
         {
+            SetTextureInterpolation(BitmapScalingMode.NearestNeighbor);
             AddScene(0, new Scene1());
             ShowScene(0);
         }
@@ -50,11 +52,16 @@ namespace TestApp
 
         public override void OnShown()
         {
-            BackgroundColor = Colors.LightSkyBlue;
-            //coroutine = new Coroutine(new CoroutineCallback(() => { Console.WriteLine("Coroutine is up!"); }));
-            //RegisterCoroutine(coroutine);
+            BackgroundColor = Colors.Black;
             GO testGO = new GO();
+            //GO1 testGO1 = new GO1();
             AddGameObject(testGO);
+        }
+
+        public override void OnUpdate()
+        {
+            if (Keyboard.IsKeyDown(Keys.A))
+                Viewport.Transform.Position += new Vector2(-1);
         }
     }
 
@@ -63,9 +70,18 @@ namespace TestApp
         int i = 0;
         public override void OnUpdate()
         {
-            if (Keyboard.IsKeyDown(Keys.W))
+            if (Keyboard.IsKeyDown(Keys.D))
                 i++;
-            this.Transform.Position = new Vector2(0, i);
+            else { i = 0; }
+            this.Transform.Position += new Vector2(i);
+        }
+    }
+    public class GO1 : GameObject
+    {
+        public override void OnCreated()
+        {
+            this.Transform.Position = new Vector2(128, 128);
+            this.Transform.Bounds = new Rect(0, 0, 128, 128);
         }
     }
 }
