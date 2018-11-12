c73c6f40379cf6d5546de538d6f604e6821a3125
Rodion <rodion.morozov.01@gmail.com>
2018-11-12 15:58:32 +0700

    Added internal engine resources package loader

--- craftersmine.EtherEngine.Core/GameApplication.cs
+++ craftersmine.EtherEngine.Core/GameApplication.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using craftersmine.EtherEngine.Content;
 
 namespace craftersmine.EtherEngine.Core
 {
@@ -14,6 +15,8 @@ namespace craftersmine.EtherEngine.Core
         internal static GameWindow GameWnd { get; set; }
         internal static GameUpdater Updater { get; set; }
         internal static string TemporaryDirectory { get; private set; }
+        internal static ContentStorage InternalResources { get; set; }
+        internal static Dictionary<string, Texture> InternalTextures { get; set; } = new Dictionary<string, Texture>();
         
         public static Logger Logger { get; private set; }
         public static bool IsExiting { get; internal set; }
@@ -25,9 +28,28 @@ namespace craftersmine.EtherEngine.Core
                 TemporaryDirectory = Environment.GetEnvironmentVariable("temp");
                 SetLogger(new Logger(TemporaryDirectory, "craftersmine_EtherEngine_Game_" + Path.GetFileName(System.Reflection.Assembly.GetExecutingAssembly().Location)));
 
-
                 Log(LogEntryType.Info, "Initializing game process...");
                 GameWnd = window;
+                Log(LogEntryType.Info, "Loading internal engine resources...");
+                InternalResources = new ContentStorage("engine.internal");
+                string[] meta = InternalResources.LoadStrings("metadata");
+                foreach (var lineMeta in meta)
+                {
+                    string[] nameType = lineMeta.Split('=');
+                    if (nameType.Length > 0)
+                    {
+                        switch (nameType[1].ToLower())
+                        {
+                            case "texture":
+                                Log(LogEntryType.Info, "InternalContentLoader: Loading texture: " + nameType[0]);
+                                InternalTextures.Add(nameType[0], InternalResources.LoadTexture(nameType[0], TextureLayout.Tile));
+                                break;
+                            default:
+                                Log(LogEntryType.Warning, "InternalContentLoader: Invalid type of resource \"" + nameType[0] + "\"! Skipping it!");
+                                break;
+                        }
+                    }
+                }
                 Log(LogEntryType.Info, "Creating renderer...");
                 Renderer = new Renderer(GameWnd.AcceleratedCanvas);
                 Log(LogEntryType.Info, "Creating game updater...");
@@ -48,9 +70,13 @@ namespace craftersmine.EtherEngine.Core
         {
             Log(LogEntryType.Info, "Exiting game...");
             Log(LogEntryType.Info, "Stopping game updater thread...");
-            Updater.StopUpdater();
+            if (Updater != null)
+                Updater.StopUpdater();
+            else Log(LogEntryType.Warning, "No game updater instance found! Game not started yet?");
             Log(LogEntryType.Info, "Stopping renderer thread...");
-            Renderer.StopRenderer();
+            if (Renderer != null)
+                Renderer.StopRenderer();
+            else Log(LogEntryType.Warning, "No game renderer instance found! Game not started yet?");
             Log(LogEntryType.Info, "Game exited! Exit code: " + exitCode + " (0x" + exitCode.ToString("X") + ")");
             Environment.Exit(exitCode);
         }
