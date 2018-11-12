869b5e26bf7915ebb012c6f1f69a804a4a75ea37
Rodion <rodion.morozov.01@gmail.com>
2018-11-12 16:00:28 +0700

    Updated renderer

--- craftersmine.EtherEngine.Core/Renderer.cs
+++ craftersmine.EtherEngine.Core/Renderer.cs
@@ -12,12 +12,14 @@ namespace craftersmine.EtherEngine.Core
         private string DebugInfoData { get; set; }
         private int FPSLastTick { get; set; }
 
+        internal SolidColorBrush SceneBgBrush { get; set; } = new SolidColorBrush(Colors.Black);
         internal AccurateTimer FrameUpdater { get; private set; }
         internal DispatcherTimer DebugInfoUpdater { get; private set; }
         internal AcceleratedCanvas AcceleratedCanvas { get; set; }
         internal Stopwatch FrameTime { get; private set; }
 
         internal Label DebugInfo { get; set; } = new Label() { Foreground = new SolidColorBrush(Color.FromRgb(0, 255,0)), Content = "" };
+        internal Grid SceneGrid { get; private set; }
 
         internal int CurrentTick { get; private set; }
         internal int FPSCounter { get; private set; }
@@ -32,7 +34,9 @@ namespace craftersmine.EtherEngine.Core
             DebugInfoUpdater.Tick += UpdateDebugInfo;
 
             FrameTime = new Stopwatch();
-
+            SceneGrid = new Grid();
+            SceneGrid.Background = SceneBgBrush;
+            AcceleratedCanvas.Canvas._base.Children.Add(SceneGrid);
             AcceleratedCanvas.Canvas._base.Children.Add(DebugInfo);
         }
 
@@ -40,11 +44,29 @@ namespace craftersmine.EtherEngine.Core
         {
             DebugInfoData = string.Format("craftersmine EtherEngine - Game debug info{0}{0}  Draw Call: {1}{0}  FPS: {2}{0}  Frame Time: {3} ms{0}  TPS: {4}", Environment.NewLine, CurrentTick, FPSCounter, FrameTime.ElapsedMilliseconds, GameApplication.Updater.TPSCounter);
             DebugInfo.Content = DebugInfoData;
-
+            if (GameApplication.GameWnd.CurrentScene != null)
+            {
+                SceneBgBrush.Color = GameApplication.GameWnd.CurrentScene.BackgroundColor;
+                _cleanSceneGrid();
+                SceneGrid.Background = SceneBgBrush;
+                if (GameApplication.GameWnd.CurrentScene.GameObjects.Count > 0)
+                {
+                    for (int gObj = 0; gObj < GameApplication.GameWnd.CurrentScene.GameObjects.Count; gObj++)
+                    {
+                        GameApplication.GameWnd.CurrentScene.GameObjects[gObj].GameObjectBase.Source = GameApplication.GameWnd.CurrentScene.GameObjects[gObj].CurrentTexture.RendererInternalTexture;
+                        SceneGrid.Children.Add(GameApplication.GameWnd.CurrentScene.GameObjects[gObj].GameObjectBase);
+                    }
+                }
+            }
             FrameTime.Reset();
             FrameTime.Start();
             if (CurrentTick == int.MaxValue) CurrentTick = 0;
             CurrentTick++;
+        }
+
+        private void _cleanSceneGrid()
+        {
+            SceneGrid.Children.Clear();
         }
 
         private void UpdateDebugInfo(object sender, EventArgs e)
