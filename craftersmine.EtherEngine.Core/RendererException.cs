b23774a7d9f5ad7cf4136baed471adf1f22a0283
Rodion <rodion.morozov.01@gmail.com>
2018-11-12 22:51:10 +0700

    Added RenderException

---
+++ craftersmine.EtherEngine.Core/RendererException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace craftersmine.EtherEngine.Core
+{
+    [Serializable]
+    internal class RendererException : Exception
+    {
+        public RendererException()
+        {
+        }
+
+        public RendererException(string message) : base(message)
+        {
+        }
+
+        public RendererException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected RendererException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
