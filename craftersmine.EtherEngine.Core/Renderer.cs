c0a1fd03cfd005579125bfbfe89b2143a83a6858
Rodion <rodion.morozov.01@gmail.com>
2018-11-12 23:04:26 +0700

    Massive updates in renderer

--- craftersmine.EtherEngine.Core/Renderer.cs
+++ craftersmine.EtherEngine.Core/Renderer.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Threading;
+using craftersmine.GameEngine.Input;
 
 namespace craftersmine.EtherEngine.Core
 {
@@ -13,6 +15,7 @@ namespace craftersmine.EtherEngine.Core
         private int FPSLastTick { get; set; }
 
         internal SolidColorBrush SceneBgBrush { get; set; } = new SolidColorBrush(Colors.Black);
+        internal SolidColorBrush GameObjBoundingsBrush { get; set; } = new SolidColorBrush(Colors.Yellow);
         internal AccurateTimer FrameUpdater { get; private set; }
         internal DispatcherTimer DebugInfoUpdater { get; private set; }
         internal AcceleratedCanvas AcceleratedCanvas { get; set; }
@@ -23,11 +26,12 @@ namespace craftersmine.EtherEngine.Core
 
         internal int CurrentTick { get; private set; }
         internal int FPSCounter { get; private set; }
+        internal BitmapScalingMode TextureInterpolation { get; set; }
 
         public Renderer(AcceleratedCanvas acceleratedCanvas)
         {
+            TextureInterpolation = BitmapScalingMode.Linear;
             AcceleratedCanvas = acceleratedCanvas;
-
             FrameUpdater = new AccurateTimer(new Action(Render), 17);
             DebugInfoUpdater = new DispatcherTimer();
             DebugInfoUpdater.Interval = TimeSpan.FromSeconds(1d);
@@ -47,14 +51,36 @@ namespace craftersmine.EtherEngine.Core
             if (GameApplication.GameWnd.CurrentScene != null)
             {
                 SceneBgBrush.Color = GameApplication.GameWnd.CurrentScene.BackgroundColor;
-                _cleanSceneGrid();
                 SceneGrid.Background = SceneBgBrush;
                 if (GameApplication.GameWnd.CurrentScene.GameObjects.Count > 0)
                 {
                     for (int gObj = 0; gObj < GameApplication.GameWnd.CurrentScene.GameObjects.Count; gObj++)
                     {
-                        GameApplication.GameWnd.CurrentScene.GameObjects[gObj].GameObjectBase.Source = GameApplication.GameWnd.CurrentScene.GameObjects[gObj].CurrentTexture.RendererInternalTexture;
-                        SceneGrid.Children.Add(GameApplication.GameWnd.CurrentScene.GameObjects[gObj].GameObjectBase);
+                        //GameApplication.Log(LogEntryType.Debug, GameApplication.GameWnd.CurrentScene.GameObjects[gObj].InternalGameName + "    X: " + GameApplication.GameWnd.CurrentScene.GameObjects[gObj].Transform.Position.X + " | Y: " + GameApplication.GameWnd.CurrentScene.GameObjects[gObj].Transform.Position.Y + " | CamX: " + GameApplication.GameWnd.CurrentScene.GameObjects[gObj].Transform.RelativeCameraPosition.X + " | CamY: " + GameApplication.GameWnd.CurrentScene.GameObjects[gObj].Transform.RelativeCameraPosition.Y, true);
+                        GameApplication.GameWnd.CurrentScene.GameObjects[gObj].GameObjectBase.Margin = new Thickness(GameApplication.GameWnd.CurrentScene.GameObjects[gObj].Transform.RelativeCameraPosition.X, GameApplication.GameWnd.CurrentScene.GameObjects[gObj].GameObjectBase.Margin.Top, GameApplication.GameWnd.CurrentScene.GameObjects[gObj].GameObjectBase.Margin.Right, GameApplication.GameWnd.CurrentScene.GameObjects[gObj].Transform.RelativeCameraPosition.Y);
+                        GameApplication.GameWnd.CurrentScene.GameObjects[gObj].GameObjectBase.Stroke = GameObjBoundingsBrush;
+
+
+                        GameApplication.GameWnd.CurrentScene.GameObjects[gObj].GameObjectBase.Fill = new ImageBrush(GameApplication.GameWnd.CurrentScene.GameObjects[gObj].CurrentTexture.RendererInternalTexture);
+
+                        RenderOptions.SetBitmapScalingMode(GameApplication.GameWnd.CurrentScene.GameObjects[gObj].GameObjectBase.Fill, TextureInterpolation);
+                        switch (GameApplication.GameWnd.CurrentScene.GameObjects[gObj].CurrentTexture.TextureLayout)
+                        {
+                            case Content.TextureLayout.Default:
+                            case Content.TextureLayout.Stretch:
+                                GameApplication.GameWnd.CurrentScene.GameObjects[gObj].GameObjectBase.Fill = new ImageBrush(GameApplication.GameWnd.CurrentScene.GameObjects[gObj].CurrentTexture.RendererInternalTexture) { Stretch = Stretch.Fill, TileMode = TileMode.None };
+                                break;
+                            case Content.TextureLayout.Tile:
+                                GameApplication.GameWnd.CurrentScene.GameObjects[gObj].GameObjectBase.Fill = new ImageBrush(GameApplication.GameWnd.CurrentScene.GameObjects[gObj].CurrentTexture.RendererInternalTexture) { Stretch = Stretch.Fill, TileMode = TileMode.Tile, ViewportUnits = BrushMappingMode.Absolute, Viewport = new Rect(0d, 0d, GameApplication.GameWnd.CurrentScene.GameObjects[gObj].CurrentTexture.RendererInternalTexture.PixelWidth, GameApplication.GameWnd.CurrentScene.GameObjects[gObj].CurrentTexture.RendererInternalTexture.PixelHeight) };
+                                break;
+                        }
+
+
+                        if (!GameApplication.GameWnd.CurrentScene.GameObjects[gObj].IsRendererProcessed)
+                        {
+                            SceneGrid.Children.Add(GameApplication.GameWnd.CurrentScene.GameObjects[gObj].GameObjectBase);
+                            GameApplication.GameWnd.CurrentScene.GameObjects[gObj].IsRendererProcessed = true;
+                        }
                     }
                 }
             }
@@ -64,9 +90,13 @@ namespace craftersmine.EtherEngine.Core
             CurrentTick++;
         }
 
-        private void _cleanSceneGrid()
+        internal void ClearScene()
         {
             SceneGrid.Children.Clear();
+            for (int gObj = 0; gObj < GameApplication.GameWnd.CurrentScene.GameObjects.Count; gObj++)
+            {
+                GameApplication.GameWnd.CurrentScene.GameObjects[gObj].IsRendererProcessed = true;
+            }
         }
 
         private void UpdateDebugInfo(object sender, EventArgs e)
