df4507be7ca10bc03dab327dc79b66456509845c
Rodion <rodion.morozov.01@gmail.com>
2018-11-12 23:07:58 +0700

    Updated Viewport

--- craftersmine.EtherEngine.Core/Viewport.cs
+++ craftersmine.EtherEngine.Core/Viewport.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace craftersmine.EtherEngine.Core
 {
     public sealed class Viewport
     {
+        internal Rect ViewportBoundings { get; set; }
+
         public Transform Transform { get; private set; }
 
         public Viewport(Transform transform)
         {
+            UpdateViewport(transform);
+        }
+
+        public void UpdateViewport(Transform transform)
+        {
             Transform = transform;
+            ViewportBoundings = new Rect(Transform.Position.X, Transform.Position.Y, Transform.Bounds.Width, Transform.Bounds.Height);
         }
     }
 }
