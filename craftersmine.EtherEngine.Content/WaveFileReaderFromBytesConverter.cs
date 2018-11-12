07d74641b33eb72d8894c1878d4f787c944e1e68
Rodion <rodion.morozov.01@gmail.com>
2018-11-12 14:19:16 +0700

    Updated sound content module

---
+++ craftersmine.EtherEngine.Content/WaveFileReaderFromBytesConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using NAudio.Wave;
+
+namespace craftersmine.EtherEngine.Content
+{
+    /// <summary>
+    /// Gives static method of conversion <code>byte</code> array into <see cref="WaveFileReader"/>
+    /// </summary>
+    public sealed class WaveFileReaderFromBytesConverter
+    {
+        /// <summary>
+        /// Converts <code>byte</code> array into <see cref="WaveFileReader"/>
+        /// </summary>
+        /// <param name="byteArrayIn">Input of <code>byte</code> array</param>
+        /// <returns>Returns <see cref="WaveFileReader"/></returns>
+        public static WaveFileReader ByteArrayToWaveFileReader(byte[] byteArrayIn)
+        {
+            WaveFileReader returnWaveReader = null;
+            if (byteArrayIn != null)
+            {
+                MemoryStream ms = new MemoryStream(byteArrayIn);
+                ms.Position = 0;
+                returnWaveReader = new WaveFileReader(ms);
+            }
+            return returnWaveReader;
+        }
+    }
+}
