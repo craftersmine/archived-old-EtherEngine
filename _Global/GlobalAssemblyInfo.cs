92745f21e1ef6d859739db10448bd0b6e4a6346e
Rodion <rodion.morozov.01@gmail.com>
2018-11-13 17:03:48 +0700

    Updated TravisCI build script

--- _Global/GlobalAssemblyInfo.cs
+++ _Global/GlobalAssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.0.{build_number}")]
+[assembly: AssemblyFileVersion("0.1.0.{build_number}")]
 [assembly: AssemblyCompany("craftersmine")]
 [assembly: AssemblyProduct("craftersmine EtherEngine")]
 [assembly: AssemblyCopyright("Copyright © craftersmine 2018")]
